0c634aeaead79be09757cd524b9c9fb56bcc9dae
Jayson <leejayson01@gmail.com>
2023-03-18 11:35:20 -0500

    Website completed

---
+++ src/components/Tech.jsx
@@ -0,0 +1,27 @@
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import { textVariant } from '../utils/motion';
+
+const Tech = () => {
+  return (
+    <>
+      <motion.div variant={textVariant()}>
+      <p className={styles.sectionSubText}>Technologies</p>
+        <h2 className={styles.sectionHeadText}>Skills</h2>
+      </motion.div>
+    <div className='h-10'></div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon}/>
+          </div>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default SectionWrapper(Tech,"");
